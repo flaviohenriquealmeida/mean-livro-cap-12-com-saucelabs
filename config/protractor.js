4a57241b33331dff85172228c1ecf2b507f78813
Flavio Almeida <flavioalmeida@MacBook-Pro-de-Flavio.local>
2015-06-04 22:47:26 -0300

    abrindo auth direto para evitar lag

--- config/protractor.js
+++ config/protractor.js
@@ -11,7 +11,7 @@ exports.config = {
   },
   specs: ['../test/e2e/**/*.js'],
   onPrepare: function() {
-    browser.driver.get('http://localhost:3000').then(function() {
+    browser.driver.get('http://localhost:3000/#/auth').then(function() {
       browser.driver.findElement(by.id('entrar')).click();
       browser.driver.findElement(by.id('login_field'))
       	.sendKeys(config.seleniumUser);
