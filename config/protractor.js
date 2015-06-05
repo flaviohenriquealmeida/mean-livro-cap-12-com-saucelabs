2984318e415c5638d2a857172825e6d5b0d320e7
Flavio Almeida <flavioalmeida@MacBook-Pro-de-Flavio.local>
2015-06-04 22:36:27 -0300

    usando promise para resolve problema de timeout

--- config/protractor.js
+++ config/protractor.js
@@ -11,12 +11,14 @@ exports.config = {
   },
   specs: ['../test/e2e/**/*.js'],
   onPrepare: function() {
-    browser.driver.get('http://localhost:3000');
-    browser.driver.findElement(by.id('entrar')).click();
-    browser.driver.findElement(by.id('login_field'))
-    	.sendKeys(config.seleniumUser);
-    browser.driver.findElement(by.id('password'))
-    	.sendKeys(config.seleniumUserPassword);
-    browser.driver.findElement(by.name('commit')).click();  
+    browser.driver.get('http://localhost:3000').then(function() {
+      browser.driver.findElement(by.id('entrar')).click();
+      browser.driver.findElement(by.id('login_field'))
+      	.sendKeys(config.seleniumUser);
+      browser.driver.findElement(by.id('password'))
+      	.sendKeys(config.seleniumUserPassword);
+      browser.driver.findElement(by.name('commit')).click();  
+    });
+    
   }
 };
