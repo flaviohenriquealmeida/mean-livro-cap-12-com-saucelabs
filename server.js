a0e46148d51053189642c18ab51bfe061598bf58
Flavio Almeida <flavioalmeida@MacBook-Pro-de-Flavio.local>
2015-06-04 22:31:15 -0300

    forçando build

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ require('./config/database')(config.db);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express Server escutando na porta ' + 
               app.get('port'));
-});
+}); 
